0405905a110c6edc124df6a91f9a1e1b1c881bdc
farrah24 <msfarrahd@gmail.com>
2021-11-15 20:06:17 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listvideos = new List<string> { "pacman", "galaga", "super mario", "balloon pop", "candycrush" };
+            var newlist = listvideos.OrderBy(X => X.Length);
+        }
+    }
+}
